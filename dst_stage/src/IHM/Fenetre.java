4d7555231a3a4cf523fed48610b5ce39a8e5c93d
inees24 <72389212+inees24@users.noreply.github.com>
2021-01-25 06:37:34 +0100

    avancement interface graphique

--- dst_stage/src/IHM/Fenetre.java
+++ dst_stage/src/IHM/Fenetre.java
@@ -1,5 +1,6 @@
 package IHM;
 
+import java.awt.*;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -25,6 +26,46 @@ public class Fenetre
 	  JLabel label = new JLabel("Attributions des places en salle de DST");
 	  panel.add(label);
 	  label.setFont(new Font("Serif", Font.BOLD, 40));
+	  label.setForeground(Color.pink);
 	  f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  
+	  
+	  JLabel label1 = new JLabel ("Veuillez selectionner les classes");
+	  panel.add(label1);
+	  label1.setFont(new Font("Serif", Font.ITALIC, 20));
+	  
+	  
+	 
+	  JButton bouton1 = new JButton("MCO1");
+	  JButton bouton2 = new JButton("MCO2");
+	  JButton bouton3 = new JButton("NDRC1");
+	  JButton bouton4 = new JButton("NDRC2");
+	  JButton bouton5 = new JButton("SIO1");
+	  JButton bouton6 = new JButton("SIO2");
+	  
+	  
+	  
+	  panel.add(bouton1);
+	  bouton1.setBounds(400, 250, 100, 50);
+	  
+	  panel.add(bouton2);
+	  bouton2.setBounds(400, 250, 100, 50);
+	  
+	  panel.add(bouton3);
+	  bouton3.setBounds(400, 250, 100, 50);
+	  
+	  panel.add(bouton4);
+	  bouton4.setBounds(400, 250, 100, 50);
+	  
+	  panel.add(bouton5);
+	  bouton5.setBounds(400, 250, 100, 50);
+	  
+	  panel.add(bouton6);
+	  bouton6.setBounds(400, 250, 100, 50);
+	  
+	  
+	  
+	  
+	  
 	 }
 }
