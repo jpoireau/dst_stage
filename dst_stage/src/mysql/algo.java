363e6c4439e99cb13e88d084021a632f6635bbb6
jpoireau <72388830+jpoireau@users.noreply.github.com>
2021-01-27 15:16:54 +0100

    table et élève aléatoire

--- dst_stage/src/mysql/algo.java
+++ dst_stage/src/mysql/algo.java
@@ -8,7 +8,8 @@ import java.sql.Statement;
 
 public class algo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 		Connection c = null;
 		  try
@@ -16,9 +17,14 @@ public class algo {
 		   Class.forName("com.mysql.cj.jdbc.Driver");
 		   String url = "jdbc:mysql://localhost/dst", user = "root", password = "";
 		   c = DriverManager.getConnection(url, user, password);
-		   String req2 = "select* from eleve where id_classe = 1 order by rand()"; // classe aléatoirement les élèves de MCO1
+		   String req2 = "select* from tables where id_colonne = 'A' order by rand()"; // classe aléatoirement les élèves de MCO1
 		   Statement s2 = c.createStatement();
 		   ResultSet rs2 = s2.executeQuery(req2);
+		   
+		   String req4 = "select prenom_eleve, nom_eleve from eleve where id_classe = '1' order by rand()";
+		   Statement s4 = c.createStatement();
+		   ResultSet rs4 = s4.executeQuery(req4);
+		   
 		   
 /*		   String req3 = "update eleve set id_colonne = '1', id_table = '1' where id_eleve = '1'";
 		   Statement s3 = c.createStatement();
@@ -27,8 +33,17 @@ public class algo {
 		   while (rs2.next())
 		   {
 		    //System.out.println(rs2.getInt(1) + " : " + rs2.getInt(2) + " : " + rs2.getString(3));
-		    String nom = rs2.getString(5);
+		    String nom = rs2.getString(2);
 		    System.out.println(nom);
+		   }
+		   
+		   while (rs4.next())
+		   {
+			   String prenom = rs4.getString(1);
+			   System.out.println(prenom);
+			   String nombre = rs4.getString(2);
+			   System.out.println(nombre);
+			   
 		   }
 		   
 		  }
