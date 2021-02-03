b8cca7d453526bcb598bcb93325ed44c1bfba380
inees24 <72389212+inees24@users.noreply.github.com>
2021-02-03 15:01:30 +0100

    tout les boutons

--- dst_stage/src/IHM/Fenetre.java
+++ dst_stage/src/IHM/Fenetre.java
@@ -98,12 +98,122 @@ public class Fenetre
 			  catch(Exception e) {System.out.println(e);
 		  }
 		  }});
+	  
 	  JButton bouton3 = new JButton("NDRC1");
+	  
+	  bouton3.addActionListener(new ActionListener(){
+		  public void actionPerformed(ActionEvent arg0) {
+			  try {
+				  Connection c = null;
+				  Class.forName("com.mysql.cj.jdbc.Driver");
+				   String url = "jdbc:mysql://localhost/dst", user = "root", password = "";
+				
+
+				   c = DriverManager.getConnection(url, user, password);
+				   
+				   String req6 = "select prenom_eleve, nom_eleve from eleve where id_classe = '3' order by rand()";
+				   Statement s6 = c.createStatement();
+				   ResultSet rs6 = s6.executeQuery(req6);
+			  
+				   while (rs6.next())
+				   {
+					   String prenom = rs6.getString(1);
+					   System.out.println(prenom);
+					   String nombre = rs6.getString(2);
+					   System.out.println(nombre);
+					   
+				   }
+			  }
+			  catch(Exception e) {System.out.println(e);
+		  }
+		  }});
+	  
 	  JButton bouton4 = new JButton("NDRC2");
+	  
+	  bouton4.addActionListener(new ActionListener(){
+		  public void actionPerformed(ActionEvent arg0) {
+			  try {
+				  Connection c = null;
+				  Class.forName("com.mysql.cj.jdbc.Driver");
+				   String url = "jdbc:mysql://localhost/dst", user = "root", password = "";
+				
+
+				   c = DriverManager.getConnection(url, user, password);
+				   
+				   String req7 = "select prenom_eleve, nom_eleve from eleve where id_classe = '4' order by rand()";
+				   Statement s7 = c.createStatement();
+				   ResultSet rs7 = s7.executeQuery(req7);
+			  
+				   while (rs7.next())
+				   {
+					   String prenom = rs7.getString(1);
+					   System.out.println(prenom);
+					   String nombre = rs7.getString(2);
+					   System.out.println(nombre);
+					   
+				   }
+			  }
+			  catch(Exception e) {System.out.println(e);
+		  }
+		  }});
+	  
 	  JButton bouton5 = new JButton("SIO1");
+	  
+	  bouton5.addActionListener(new ActionListener(){
+		  public void actionPerformed(ActionEvent arg0) {
+			  try {
+				  Connection c = null;
+				  Class.forName("com.mysql.cj.jdbc.Driver");
+				   String url = "jdbc:mysql://localhost/dst", user = "root", password = "";
+				
+
+				   c = DriverManager.getConnection(url, user, password);
+				   
+				   String req8 = "select prenom_eleve, nom_eleve from eleve where id_classe = '5' order by rand()";
+				   Statement s8 = c.createStatement();
+				   ResultSet rs8 = s8.executeQuery(req8);
+			  
+				   while (rs8.next())
+				   {
+					   String prenom = rs8.getString(1);
+					   System.out.println(prenom);
+					   String nombre = rs8.getString(2);
+					   System.out.println(nombre);
+					   
+				   }
+			  }
+			  catch(Exception e) {System.out.println(e);
+		  }
+		  }});
+	  
 	  JButton bouton6 = new JButton("SIO2");
 	  
-	  
+	  bouton6.addActionListener(new ActionListener(){
+		  public void actionPerformed(ActionEvent arg0) {
+			  try {
+				  Connection c = null;
+				  Class.forName("com.mysql.cj.jdbc.Driver");
+				   String url = "jdbc:mysql://localhost/dst", user = "root", password = "";
+				
+
+				   c = DriverManager.getConnection(url, user, password);
+				   
+				   String req9 = "select prenom_eleve, nom_eleve from eleve where id_classe = '6' order by rand()";
+				   Statement s9 = c.createStatement();
+				   ResultSet rs9 = s9.executeQuery(req9);
+			  
+				   while (rs9.next())
+				   {
+					   String prenom = rs9.getString(1);
+					   System.out.println(prenom);
+					   String nombre = rs9.getString(2);
+					   System.out.println(nombre);
+					   
+				   }
+			  }
+			  catch(Exception e) {System.out.println(e);
+		  }
+		  }});
 	  
 	  panel.add(bouton1);
 	  bouton1.setBounds(400, 250, 100, 50);
